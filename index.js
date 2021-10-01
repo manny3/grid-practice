813f44fb4f7509185504c692c1ec47c1bea7349a
wilbert-chen <wilbert.chen@udngroup.com.tw>
2021-10-01 11:53:09 +0800

    add Grid-flow

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const switcher = document.getElementById("switcher");
-const root = document.documentElement;
+let switcher = document.getElementById("switcher");
+let switcher2 = document.getElementById("switcher2");
+let root = document.documentElement;
 
 switcher.addEventListener("change", function (evt) {
-  root.style.setProperty("--var-repeat", evt.target.value);
+   root.style.setProperty('--var-flow', evt.target.value);
+});
+
+switcher2.addEventListener("change", function (evt) {
+   root.style.setProperty('--var-mode', evt.target.value);
+});
+
+
+let packing = document.getElementById("gridPacking");
+packing.addEventListener("click", function (evt) {
+  if(packing.checked) {
+    root.style.setProperty('--var-packing', 'row dense');
+  } else {
+    root.style.setProperty('--var-packing', 'row');
+  }
 });
