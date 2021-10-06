0489c06a25bfac698bef960fdfe3ad778783655d
wilbert-chen <wilbert.chen@udngroup.com.tw>
2021-10-06 16:03:41 +0800

    add alignment-flex

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 let root = document.documentElement;
-let switcher = document.getElementById("switcher");
-let direction = document.getElementById("direction");
 
-switcher.addEventListener("change", function (evt) {
-  root.style.setProperty('--var-writing-mode', evt.target.value);
+
+let switcher3= document.getElementById("switcher3");
+switcher3.addEventListener("change", function (evt) {
+  root.style.setProperty('--var-align-content', evt.target.value);
 });
 
-direction.addEventListener("click", function (evt) {
-  if(direction.checked) {
-    root.style.setProperty('--var-direction', 'rtl');
-  } else {
-    root.style.setProperty('--var-direction', 'ltr');
-  }
+let switcher4= document.getElementById("switcher4");
+switcher4.addEventListener("change", function (evt) {
+  root.style.setProperty('--var-justify-content', evt.target.value);
 });
 
+let switcher5= document.getElementById("switcher5");
+switcher5.addEventListener("change", function (evt) {
+  root.style.setProperty('--var-align-items', evt.target.value);
+});
 
-// let switcher3= document.getElementById("switcher3");
-// switcher3.addEventListener("change", function (evt) {
-//   root.style.setProperty('--var-align-content', evt.target.value);
-// });
-
-// let switcher4= document.getElementById("switcher4");
-// switcher4.addEventListener("change", function (evt) {
-//   root.style.setProperty('--var-justify-content', evt.target.value);
-// });
+let switcher6= document.getElementById("switcher6");
+switcher6.addEventListener("change", function (evt) {
+  root.style.setProperty('--var-justify-items', evt.target.value);
+});
